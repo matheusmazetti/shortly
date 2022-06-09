9ebd4b04f83e77496a84acbf3c64b6fb81381bfe
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-06-09 00:50:29 -0300

    delete shorten

--- Controllers/urls.js
+++ Controllers/urls.js
@@ -19,3 +19,28 @@ export const short = async (req, res) => {
         console.log(e);
     }
 };
+
+export const deleteShort = async (req, res) => {
+    let id = req.params.id;
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    try{
+        let verify = await connection.query("SELECT * FROM sessions WHERE token = $1", [token])
+        if(verify.rows.length !== 0){
+            let shorten = await connection.query("SELECT * FROM urls WHERE id = $1", [id]);
+            if(shorten.rows.length === 0){
+                res.sendStatus(404);
+            } else if(shorten.rows[0].userId !== verify.rows[0].userId){
+                res.sendStatus(401);
+            } else {
+                await connection.query("DELETE FROM urls WHERE id = $1", [id]);
+                res.sendStatus(204);
+            }
+        } else {
+            res.sendStatus(401);
+        }
+    } catch(e){
+        res.sendStatus(500);
+        console.log(e);
+    }
+};
