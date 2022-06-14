90ffc027add117a57610134e8cd4809f880f78f3
Vinicius <vinigugelmin@hotmail.com>
2022-06-14 11:36:25 -0300

    Preparando revisão de código

--- Routers/singin.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { signin } from '../Controllers/signin.js';
-import { checkSignin } from '../Middlewares/signin.js';
-
-const signinRouter = Router();
-
-signinRouter.post('/signin', checkSignin, signin);
-
-export default signinRouter;
--- Schemas/signin.js
+++
@@ -1,6 +0,0 @@
-import joi from 'joi';
-
-export const signinSchema = joi.object({
-    email: joi.string().pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/).required(),
-    password: joi.string().min(1).required()
-});
